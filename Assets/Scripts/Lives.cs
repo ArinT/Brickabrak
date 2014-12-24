0350d95fe5aaa5092a06b891a4f9126375528026
ArinT <adt608@hotmail.com>
2014-12-24 13:54:28 -0600

    Menu

---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lives : MonoBehaviour {
+
+	
+	SpriteRenderer liveOne;
+	SpriteRenderer liveTwo;
+	SpriteRenderer liveThree;
+	public Sprite onSprite;
+	public Sprite offSprite;
+	public int lives;
+
+	// Use this for initialization
+	void Start () {
+		liveOne = GameObject.FindGameObjectWithTag ("Live1").GetComponent<SpriteRenderer> ();
+		liveTwo = GameObject.FindGameObjectWithTag ("Live2").GetComponent<SpriteRenderer> ();
+		liveThree = GameObject.FindGameObjectWithTag ("Live3").GetComponent<SpriteRenderer> ();
+		lives = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameState> ().lives;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lives = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameState> ().lives;
+		if (lives >= 3)
+		{
+			liveOne.sprite = onSprite;
+			liveTwo.sprite = onSprite;
+			liveThree.sprite = onSprite; 
+		}
+		else if (lives ==2){
+			liveOne.sprite = onSprite;
+			liveTwo.sprite = onSprite;
+			liveThree.sprite = offSprite; 
+		}
+		else if (lives ==1){
+			liveOne.sprite = onSprite;
+			liveTwo.sprite = offSprite;
+			liveThree.sprite = offSprite; 
+		}
+		else{
+			liveOne.sprite = offSprite;
+			liveTwo.sprite = offSprite;
+			liveThree.sprite = offSprite; 
+		}
+	}
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	public int pos = 0;
+	public AudioSource cancel;
+	public AudioSource confirm;
+	public AudioSource move;
+
+	public Sprite[] menu_sprites = new Sprite[3];
+	/*
+	 * 0 = Play
+	 * 1 = Controls
+	 * 2 = Credits
+	 */
+	void Start () {
+		pos = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			pos++;pos = pos%3;
+			move.Play();
+		}
+		else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			pos--;
+			pos = pos<0?pos+3:pos;
+			move.Play();
+		}
+		else if (Input.GetButtonDown ("Submit")){
+			if (pos == 0)
+			{
+				confirm.Play ();
+				Application.LoadLevel("Level 2 V1");
+			}
+		}
+		GetComponent<UnityEngine.UI.Image> ().sprite = menu_sprites[pos];
+	}
+}
